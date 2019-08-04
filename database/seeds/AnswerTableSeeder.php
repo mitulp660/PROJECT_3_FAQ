44ce202d8236f23135173362bd955e26cde4107d
mitul <mitulp660@gmail.com>
2019-08-04 16:19:34 -0400

    Completed creating home page to display questions.

--- database/seeds/AnswerTableSeeder.php
+++ database/seeds/AnswerTableSeeder.php
@@ -14,7 +14,7 @@ class AnswerTableSeeder extends Seeder
         $users = App\User::all();
         for ($i = 1; $i <= 6; $i++) {
             $users->each(function ($user) {
-                $question = App\Question::inOrder()->first();
+                $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
                 $answer->user()->associate($user);
                 $answer->question()->associate($question);
